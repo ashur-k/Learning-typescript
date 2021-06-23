e746fa455f52b96c19b4628fae917127e88a3776
ashur-k <ashurkanwal@yahoo.com>
2021-06-23 10:57:03 +0100

    seeing error catching by typescript in action

--- index.ts
+++ index.ts
@@ -3,6 +3,31 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  const todo = response.data;
-  
+  // Response.dat has properties of@
+  // id
+  // title
+  // completed
+
+  // Writing typescript
+  // interface in typescript are used to define a structure of an object
+
+  interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+
+
+  const todo = response.data as Todo;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
+
 });
